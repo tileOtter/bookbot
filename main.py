6f26c0df508eafc556fec87f0b903d212f4495d9
tileotter <adamfoster913@gmail.com>
2025-09-13 20:13:53 -0400

    defined get_book_text and main -- main returns a string using get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
+def main():
+    return get_book_text("books/frankenstein.txt")
+print(main())
