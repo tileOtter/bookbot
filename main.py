b05a6389962b9260004ee98638f40afe7ce5db10
tileotter <adamfoster913@gmail.com>
2025-09-13 20:36:22 -0400

    word_count defined

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def get_book_text(path):
     with open(path) as book:
         return book.read()
+    
 def main():
     return get_book_text("books/frankenstein.txt")
-print(main())
+
+words = main().split()
+
+def word_count(words):
+    return len(words)
+
+print(f"{word_count(words)} words found in the document.")       
+
+
