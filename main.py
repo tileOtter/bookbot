50b2447e90a11e512d0dfabc521c6590afd7863f
tileotter <adamfoster913@gmail.com>
2025-09-16 13:19:17 -0400

    refactoring

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ def get_book_text(path):
 def main():
     return get_book_text("books/frankenstein.txt")
 
+from stats import word_count
+
 words = main().split()
 
-def word_count(words):
-    return len(words)
-
 print(f"{word_count(words)} words found in the document.")       
-
-
---
+++ stats.py
@@ -0,0 +1,2 @@
+def word_count(words):
+    return len(words)
