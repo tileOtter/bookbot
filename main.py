c455d0cfab43f8bcdf0507d288b7580eebb2e027
tileotter <adamfoster913@gmail.com>
2025-09-16 13:46:05 -0400

    added character count output to stats.py and calls for it in main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def get_book_text(path):
 def main():
     return get_book_text("books/frankenstein.txt")
 
-from stats import word_count
+from stats import word_count, character_count
 
 words = main().split()
 
-print(f"{word_count(words)} words found in the document.")       
+print(f"{word_count(words)} words found in the document.")   
+print(character_count(main))    
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def word_count(words):
     return len(words)
+
+def character_count(main):
+    characters = {}
+    lowercase = main().lower()
+    for letter in lowercase:
+        if letter not in characters:
+            characters[letter] = 1
+        else:
+            characters[letter] += 1
+
+    return characters 
