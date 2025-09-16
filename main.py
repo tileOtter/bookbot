2f92ea21e0e30ac423d890fb879f6a769fc58909
tileotter <adamfoster913@gmail.com>
2025-09-16 15:39:02 -0400

    changed the program to accept a second argument with a path to the book to be analyzed. additional books can be added to the /books directory to be scanned

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-def get_book_text(path):
-    with open(path) as book:
-        return book.read()
-    
-def main():
-    return get_book_text("books/frankenstein.txt")
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    def get_book_text(path):
+        with open(path) as book:
+            return book.read()
+        
+    def main():
+        return get_book_text(sys.argv[1])
 
-book_fp = "books/frankenstein.txt"
-from stats import word_count, character_count, sorted_cc
+    book_fp = sys.argv[1]
 
-words = main().split()
-cc = character_count(main)
-sortedchar = sorted_cc(cc)
+    from stats import word_count, character_count, sorted_cc
 
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at {book_fp}...")
-print("----------- Word Count ----------")
-print(f"Found {word_count(words)} total words")  
-print("--------- Character Count -------")    
-for char in sortedchar:
-    if char["character"].isalpha() is True:
-        print(f"{char["character"]}: {char["count"]}")
-print("============= END ===============")
+    words = main().split()
+    cc = character_count(main)
+    sortedchar = sorted_cc(cc)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_fp}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count(words)} total words")  
+    print("--------- Character Count -------")    
+    for char in sortedchar:
+        if char["character"].isalpha() is True:
+            print(f"{char["character"]}: {char["count"]}")
+    print("============= END ===============")
 
