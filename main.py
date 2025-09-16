b664f1d6e64279da2a36afe543a8e7f1ae1abf49
tileotter <adamfoster913@gmail.com>
2025-09-16 15:26:26 -0400

    sorted characters by count in sorted_cc, and prettied up the output, which now only gives count of alphabetical characters

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ def get_book_text(path):
 def main():
     return get_book_text("books/frankenstein.txt")
 
-from stats import word_count, character_count
+book_fp = "books/frankenstein.txt"
+from stats import word_count, character_count, sorted_cc
 
 words = main().split()
+cc = character_count(main)
+sortedchar = sorted_cc(cc)
 
-print(f"{word_count(words)} words found in the document.")   
-print(character_count(main))    
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_fp}...")
+print("----------- Word Count ----------")
+print(f"Found {word_count(words)} total words")  
+print("--------- Character Count -------")    
+for char in sortedchar:
+    if char["character"].isalpha() is True:
+        print(f"{char["character"]}: {char["count"]}")
+print("============= END ===============")
+
--- stats.py
+++ stats.py
@@ -10,4 +10,21 @@ def character_count(main):
         else:
             characters[letter] += 1
 
-    return characters 
+    return characters
+
+def sorted_cc(characters):
+    def sort_on(characters):
+        return characters["count"]
+    
+    sorted = []
+    
+    for character in characters:
+        count = characters[character]
+        cc = {
+            "character": character,
+            "count": count
+        }
+        sorted.append(cc)
+
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
